ba71e3949cd4176b176b083d38e7847bd2db903e
unknown <Adeleyeha.17@protonmail.com>
2023-06-12 16:24:35 +0100

    added the course component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Course } from "./component/Course";
 import { Header } from "./component/Header";
 
 
@@ -7,7 +8,7 @@ function App() {
    
       <Header />
       
-
+      <Course />
     </div>
   );
 }
---
+++ src/component/Course.jsx
@@ -0,0 +1,26 @@
+
+export const Course = () => {
+  return (
+    <div>
+      <div className="course">
+      <h1>Course We Offer</h1>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et voluptate atque officia minima. Nostrum porro totam ipsum molestias tempora maxime dolor ab, quam, a deserunt ad sed corrupti, ducimus ipsa.</p>
+      </div>
+
+      <div className="row">
+         <div className="course-col">
+            <h3>Intermediate</h3>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa aperiam possimus eos, reiciendis, ipsam impedit eum facere perferendis, suscipit sapiente tempore! Tempore sed repudiandae natus omnis commodi nam, quasi deleniti.</p>
+         </div>
+         <div className="course-col">
+            <h3>Degree</h3>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa aperiam possimus eos, reiciendis, ipsam impedit eum facere perferendis, suscipit sapiente tempore! Tempore sed repudiandae natus omnis commodi nam, quasi deleniti.</p>
+         </div>
+         <div className="course-col">
+            <h3>Post Graduate</h3>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa aperiam possimus eos, reiciendis, ipsam impedit eum facere perferendis, suscipit sapiente tempore! Tempore sed repudiandae natus omnis commodi nam, quasi deleniti.</p>
+         </div>
+      </div>
+    </div>
+  )
+}
