f3abd5eedabd1670883285bc79b3431794f46dd9
unknown <Adeleyeha.17@protonmail.com>
2023-06-13 15:44:46 +0100

    completed the footer section

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -3,7 +3,15 @@ export const Footer = () => {
   return (
     <div>
       <div className="footer">
-         
+         <h4>About Us</h4>
+         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi maiores, consectetur natus odio maxime.</p>
+
+         <div className="icons">
+            <img src="facebook.png" className="socials-icon" />
+            <img src="instagram.png" className="socials-icon" />
+            <img src="twitter.png" className="socials-icon" />
+            <img src="linkedin.png" className="socials-icon" />
+         </div>
       </div>
     </div>
   )
