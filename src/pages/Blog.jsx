17b8c87bc77a1c321d1dbd95ffcc254d05df79dc
unknown <Adeleyeha.17@protonmail.com>
2023-06-15 09:55:33 +0100

    started working on the blogs page

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -3,6 +3,18 @@ import { Footer } from "../component/Footer"
 export const Blog = () => {
   return (
     <div>
+
+      <h1 id="text-box">Our Ceritifcate & Online Programs for 2023</h1>
+
+      <div className="blog-content">
+        <div className="row">
+          <div className="blog-left">
+            <img src="certificate.png" alt="" />
+            <h2>Our Ceritifcate & Online Programs for 2023</h2>
+          </div>
+          <div className="blog-right"></div>
+        </div>
+      </div>
       <Footer />
     </div>
   )
