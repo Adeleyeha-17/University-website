0c7a4b41457fdee38a0bc4a8a15ef540a82c569a
unknown <Adeleyeha.17@protonmail.com>
2023-06-15 09:38:17 +0100

    completed the courses page

--- src/pages/Courses.jsx
+++ src/pages/Courses.jsx
@@ -1,6 +1,16 @@
-
+import { Footer } from "../component/Footer"
+import { Course } from "../component/Course"
+import { Facilities } from "../component/Facilities"
 export const Courses = () => {
   return (
-    <div>Courses</div>
+    <div>
+      <h1 id="text-box">Our Courses</h1>
+
+      <Course />
+
+      <Facilities />
+      
+      <Footer />
+    </div>
   )
 }
