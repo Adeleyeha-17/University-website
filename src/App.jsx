d9919a885c4137615ff099aff60c437c1f3ce5fe
unknown <Adeleyeha.17@protonmail.com>
2023-06-20 02:27:44 +0100

    updated the router file

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,18 @@
-import {HashRouter, Routes, Route} from "react-router-dom";
-import { Home } from "./pages/Home";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { Header } from "./component/Header";
-import { About } from "./pages/About";
-import { Courses } from "./pages/Courses";
-import { Blog } from "./pages/Blog";
-import { Contact } from "./pages/Contact";
+import { routes } from "./route/RoutePage";
 
 function App() {
   return (
     <div className="app">
-   
-   <HashRouter>
-    <Header />
-    <Routes>
-      <Route path="/" element={<Home />}/>
-      <Route path="/about" element={<About />} />
-      <Route path="/courses" element={<Courses />} />
-      <Route path="/blog" element={<Blog />} />
-      <Route path="/contact" element={<Contact />} />
-    </Routes>
-   </HashRouter>
-      
+      <HashRouter>
+        <Header />
+        <Routes>
+          {routes.map((route, index) => (
+            <Route key={index} path={route.path} element={route.element} />
+          ))}
+        </Routes>
+      </HashRouter>
     </div>
   );
 }
